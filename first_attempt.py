bc1c317ab82e22a83312c9d791035ba9b9c3eb94
Conor Evans <coevans@tcd.ie>
2019-11-06 23:46:51 +0000

    use better method for fillna and test small city theory - slight improvement, -360 MAE

--- first_attempt.py
+++ first_attempt.py
@@ -9,11 +9,28 @@ from sklearn.compose import ColumnTransformer
 from catboost import CatBoostRegressor
 
 model_frame = pd.read_csv('data/train.csv')
+target_frame = pd.read_csv('data/test.csv').fillna(method='ffill')
 
-target_columns = ['Year of Record', 'Gender', 'Crime Level in the City of Employement','Satisfation with employer', 'Country', 'Age', 'Profession', 'University Degree', 'Size of City', 'Yearly Income in addition to Salary (e.g. Rental Income)']
+# fill NaN values in categorical fields with not_given
+for column in model_frame.select_dtypes(include=['object']):
+  # we want to mark categorical values as not_given (missing, non_present, whatever preferred term)
+  # as there may be a connection between values not being given and other target variables (in our case, Income)
+  # people from country X may be more reticent to share salary, height, whatever.
+  # best not to ffill 
+  model_frame[column] = model_frame[column].fillna('not_given')
 
-for column in target_columns:
-  model_frame[column] = model_frame[column].fillna(method='ffill')
+for column in model_frame.select_dtypes(exclude=['object']).drop('Instance', axis=1):
+  # cut bottom and top 10 percent of data to have a more balanced mean used to fill N/A values
+  lower = np.percentile(model_frame[column].dropna(),10)
+  upper = np.percentile(model_frame[column].dropna(),90)
+  mean = model_frame[model_frame[column].between(lower,upper)][column].values.mean()
+  model_frame[column] = model_frame[column].fillna(mean)
+
+
+model_frame['Small City'] = model_frame['Size of City'] <= 3000
+target_frame['Small City'] = target_frame['Size of City'] <= 3000
+    
+target_columns = ['Year of Record', 'Gender', 'Crime Level in the City of Employement','Satisfation with employer', 'Country', 'Age', 'Profession', 'University Degree', 'Small City', 'Size of City', 'Yearly Income in addition to Salary (e.g. Rental Income)']
 
 independent_vars = model_frame[target_columns]
 dependent_var = model_frame['Total Yearly Income [EUR]'].values
@@ -28,8 +45,6 @@ regr = Pipeline(steps=[('enc', TargetEncoder()),
 X_train, X_test, Y_train, Y_test = train_test_split(independent_vars, dependent_var, train_size = 0.8, test_size = 0.2)
 
 regr.fit(X_train, Y_train)
-
-target_frame = pd.read_csv('data/test.csv')[target_columns].fillna(method='ffill')
 
 y_predict = regr.predict(target_frame[target_columns])
 print(metrics.mean_absolute_error(Y_test, regr.predict(X_test)))
