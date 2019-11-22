45173126a4673e542ff753ad13569dc0c7cf8c53
Conor Evans <coevans@tcd.ie>
2019-11-22 18:41:00 +0000

    add comments, rename incorrectly named pearson correl coeff and p_value

--- conor_lgb.py
+++ conor_lgb.py
@@ -1,3 +1,4 @@
+# imports
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -7,11 +8,11 @@ from category_encoders import TargetEncoder
 from scipy.stats import stats
 from lightgbm import LGBMRegressor
 
+# read in data
 model_frame = pd.read_csv('data/train.csv')
 target_frame = pd.read_csv('data/test.csv')
 
 def treat_gender(frame):
-  frame['Gender'] = frame['Gender'].replace({'unknown': 'not_given'})
   frame['Gender'] = frame['Gender'].replace({'f': 'female'})
   return frame
 
@@ -23,39 +24,44 @@ def treat_work_experience(frame):
     frame['Work Experience in Current Job [years]'] = pd.to_numeric(frame['Work Experience in Current Job [years]'], errors='coerce')
     return frame
 
+# remove EUR from additional income input i.e. change '2555.12 EUR' to 2555.12
 def treat_additional_income(frame):
   frame['Yearly Income in addition to Salary (e.g. Rental Income)'] = frame['Yearly Income in addition to Salary (e.g. Rental Income)'].map(lambda x: float(x.rstrip('EUR')))
   return frame
 
+# 'Year of Record' appears to be ordered
+# give all the n/a values are at the end of the file, we backwards fill
 def treat_year_of_record(frame):
   frame['Year of Record'] = frame['Year of Record'].fillna(method='bfill')
   return frame
 
 def treat_country(model_frame, target_frame):
-  model_frame['country-z_score'] = model_frame['Country']
-  model_frame['country-p_score'] = model_frame['Country']
+  model_frame['country-pcc'] = model_frame['Country']
+  model_frame['country-p_value'] = model_frame['Country']
 
-  target_frame['country-z_score'] = target_frame['Country']
-  target_frame['country-p_score'] = target_frame['Country']
-  p_dict = {}
-  z_dict = {}
+  target_frame['country-pcc'] = target_frame['Country']
+  target_frame['country-p_value'] = target_frame['Country']
+  pcc_dict, pval_dict = {}, {}
 
   for country in model_frame['Country'].unique():
-    p_score, z_score = stats.pearsonr(model_frame['Country'] == country, model_frame['Total Yearly Income [EUR]'])
-    unrelated = z_score > 0.05
-    z_dict[country] = str(unrelated)
-    p_dict[country] = p_score
+    # scipy.stats.pearsonr(x, y)
+    # Calculates a Pearson correlation coefficient and the p-value for testing non-correlation.
+    pcc, p_value = stats.pearsonr(model_frame['Country'] == country, model_frame['Total Yearly Income [EUR]'])
+    unrelated = p_value > 0.05
+    pcc_dict[country] = pcc
+    # encode as string because we will have three values: {'True', 'False', 'unknown'}
+    pval_dict[country] = str(unrelated)
 
-    model_frame.loc[model_frame['Country'] == country, 'country-z_score'] = str(unrelated)
-    model_frame.loc[model_frame['Country'] == country, 'country-p_score'] = float(p_score)
+    model_frame.loc[model_frame['Country'] == country, 'country-pcc'] = float(pcc)
+    model_frame.loc[model_frame['Country'] == country, 'country-p_value'] = str(unrelated)
 
   for country in target_frame['Country'].unique():
-    if country in z_dict:
-      target_frame.loc[target_frame['Country'] == country, 'country-z_score'] = z_dict[country]
-      target_frame.loc[target_frame['Country'] == country, 'country-p_score'] = p_dict[country]
+    if country in pcc_dict:
+      target_frame.loc[target_frame['Country'] == country, 'country-pcc'] = pcc_dict[country]
+      target_frame.loc[target_frame['Country'] == country, 'country-p_value'] = pval_dict[country]
     else:
-      target_frame.loc[target_frame['Country'] == country, 'country-z_score'] = 'unknown'
-      target_frame.loc[target_frame['Country'] == country, 'country-p_score'] = 'nA'
+      target_frame.loc[target_frame['Country'] == country, 'country-pcc'] = 'nA'
+      target_frame.loc[target_frame['Country'] == country, 'country-p_value'] = 'unknown'
 
   model_frame = model_frame.drop('Country', axis=1)
   target_frame = target_frame.drop('Country', axis=1)
@@ -63,31 +69,31 @@ def treat_country(model_frame, target_frame):
 
   return [model_frame, target_frame]
 
+# same as country
 def treat_profession(model_frame, target_frame):
-  model_frame['profession-z_score'] = model_frame['Profession']
-  model_frame['profession-p_score'] = model_frame['Profession']
+  model_frame['profession-pcc'] = model_frame['Profession']
+  model_frame['profession-p_value'] = model_frame['Profession']
 
-  target_frame['profession-z_score'] = target_frame['Profession']
-  target_frame['profession-p_score'] = target_frame['Profession']
-  p_dict = {}
-  z_dict = {}
+  target_frame['profession-pcc'] = target_frame['Profession']
+  target_frame['profession-p_value'] = target_frame['Profession']
+  pcc_dict, pval_dict = {}, {}
 
   for profession in model_frame['Profession'].unique():
-    p_score, z_score = stats.pearsonr(model_frame['Profession'] == profession, model_frame['Total Yearly Income [EUR]'])
-    unrelated = z_score > 0.05
-    z_dict[profession] = str(unrelated)
-    p_dict[profession] = p_score
+    pcc, p_value = stats.pearsonr(model_frame['Profession'] == profession, model_frame['Total Yearly Income [EUR]'])
+    unrelated = p_value > 0.05
+    pcc_dict[profession] = pcc
+    pval_dict[profession] = str(unrelated)
 
-    model_frame.loc[model_frame['Profession'] == profession, 'profession-z_score'] = str(unrelated)
-    model_frame.loc[model_frame['Profession'] == profession, 'profession-p_score'] = float(p_score)
+    model_frame.loc[model_frame['Profession'] == profession, 'profession-pcc'] = float(pcc)
+    model_frame.loc[model_frame['Profession'] == profession, 'profession-p_value'] = str(unrelated)
 
   for profession in target_frame['Profession'].unique():
-    if profession in z_dict:
-      target_frame.loc[target_frame['Profession'] == profession, 'profession-z_score'] = z_dict[profession]
-      target_frame.loc[target_frame['Profession'] == profession, 'profession-p_score'] = p_dict[profession]
+    if profession in pcc_dict:
+      target_frame.loc[target_frame['Profession'] == profession, 'profession-pcc'] = pcc_dict[profession]
+      target_frame.loc[target_frame['Profession'] == profession, 'profession-p_value'] = pval_dict[profession]
     else:
-      target_frame.loc[target_frame['Profession'] == profession, 'profession-z_score'] = 'unknown'
-      target_frame.loc[target_frame['Profession'] == profession, 'profession-p_score'] = 'nA'
+      target_frame.loc[target_frame['Profession'] == profession, 'profession-pcc'] = 'nA'
+      target_frame.loc[target_frame['Profession'] == profession, 'profession-p_value'] = 'unknown'
 
   model_frame = model_frame.drop('Profession', axis=1)
   target_frame = target_frame.drop('Profession', axis=1)
@@ -95,6 +101,12 @@ def treat_profession(model_frame, target_frame):
 
   return [model_frame, target_frame]
 
+# unify the way categorical n/a values are encoded
+# e.g. gender has 'unknown'
+def fillna_categorical(frame):
+  for column in frame.select_dtypes(include='object'):
+    frame[column] = frame[column].fillna('unknown')
+  return frame
 
 def preprocess(frame):
   frame = treat_gender(frame)
@@ -103,6 +115,7 @@ def preprocess(frame):
   frame = treat_additional_income(frame)
   frame = treat_year_of_record(frame)
   frame = frame.replace('#NUM!', np.NaN)
+  frame = fillna_categorical(frame)
   frame = frame.fillna(method = 'bfill')
   return frame
 
@@ -115,9 +128,12 @@ model_frame, target_frame = treat_profession(model_frame, target_frame)
 model_frame['Small City'] = model_frame['Size of City'] <= 3000
 target_frame['Small City'] = target_frame['Size of City'] <= 3000
     
-target_columns = ['Work Experience in Current Job [years]','Year of Record', 'Gender', 'Crime Level in the City of Employement', 'country-p_score', 'Age', 'profession-p_score', 'University Degree', 'Small City', 'Size of City', 'Yearly Income in addition to Salary (e.g. Rental Income)', 'country-z_score', 'profession-z_score']
+target_columns = ['Work Experience in Current Job [years]','Year of Record', 'Gender', 'Crime Level in the City of Employement', 
+                  'country-pcc', 'country-p_value', 'Age', 'University Degree', 'Small City', 'Size of City', 
+                  'Yearly Income in addition to Salary (e.g. Rental Income)', 'profession-pcc', 'profession-p_value']
 
 independent_vars = model_frame[target_columns]
+# scale our target variable
 dependent_var = model_frame['Total Yearly Income [EUR]'].apply(np.log).values
 
 gcsv = GridSearchCV(estimator = LGBMRegressor(random_state=15000, num_leaves=4200),
@@ -143,5 +159,3 @@ f.write("Instance,Total Yearly Income [EUR]\n")
 
 for i in range(len(y_predict)):
   f.write(str(instances[i]) + "," + str(y_predict[i]) + "\n")
-
-print('submission complete')
